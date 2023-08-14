27bf0b6d5f665724adfd10578ed8958a4fb1fbb8
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-08-14 22:57:42 +0200

    add experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import vvmd from "./company/vvmd.png";
+import pettersonapps from "./company/pettersonapps.png";
+import malevich from "./company/malevich.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  vvmd,
+  pettersonapps,
+  malevich,
   carrent,
   jobit,
   tripguide,
